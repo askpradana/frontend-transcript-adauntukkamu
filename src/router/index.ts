fc624eec646eff1fb57f54bb7a95cd07a2f9edea
naufal <askpradana@gmail.com>
2024-10-18 20:22:34 +0700

    feat: guard /dashboard if user not loggedin yet

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { usePocketbaseStore } from '@/stores/pocketbase'
 import HomeView from '../views/HomeView.vue'
-import DashboardViewVue from '@/views/DashboardView.vue'
-import SettingsViewVue from '@/views/SettingsView.vue'
-import ProfileViewVue from '@/views/ProfileView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import SettingsView from '../views/SettingsView.vue'
+import ProfileView from '../views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,25 +16,30 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: DashboardViewVue,
+      component: DashboardView,
+      beforeEnter: (to, from, next) => {
+        const pocketbaseStore = usePocketbaseStore()
+        if (!pocketbaseStore.pb.authStore.isValid) {
+          next('/')
+        } else {
+          next()
+        }
+      },
       children: [
         {
-          path: '/settings',
+          path: 'settings',
           name: 'settings',
-          component: SettingsViewVue
+          component: SettingsView
         },
         {
-          path: '/profile',
+          path: 'profile',
           name: 'profile',
-          component: ProfileViewVue
+          component: ProfileView
         }
       ]
     }
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,13 +1,22 @@
 <script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { usePocketbaseStore } from '@/stores/pocketbase'
 import LeftBar from '@/components/loggedin/LeftBar.vue'
+
+const router = useRouter()
+const pocketbaseStore = usePocketbaseStore()
+
+onMounted(() => {
+  if (!pocketbaseStore.pb.authStore.isValid) {
+    // User is not logged in, redirect to home or login page
+    router.push('/')
+  }
+})
 </script>
 
 <template>
-  <main class="flex flex-row">
-    <!-- <p>Dashboard gengs</p>
-      <p>Loggedin as, {{ store.pb.authStore.model?.email }}</p>
-      <p>Hello, {{ store.pb.authStore.model?.name }}</p>
-      <button @click="logout">Logout</button> -->
+  <main v-if="pocketbaseStore.pb.authStore.isValid" class="flex flex-row">
     <LeftBar />
     <RouterView />
   </main>
