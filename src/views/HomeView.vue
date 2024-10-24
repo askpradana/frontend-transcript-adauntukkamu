354abd7da76163f6eca97f2c8eda5b2cb7fb25af
naufal <askpradana@gmail.com>
2024-10-24 20:23:55 +0700

    feat: create a style themes and use it on homeview.vue header banner and benefit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,21 @@ import Benefit from '../components/Benefit.vue'
 import Pricing from '../components/Pricing.vue'
 import FAQ from '../components/Faq.vue'
 import Footer from '../components/Footer.vue'
+import router from '@/router'
+import { usePocketbaseStore } from '@/stores/pocketbase'
+
+const pocketbaseStore = usePocketbaseStore()
+
+function loginWithGoogle() {
+  pocketbaseStore
+    .doLogin()
+    .then(() => {
+      router.push('/dashboard')
+    })
+    .catch(error => {
+      console.error('Login failed:', error)
+    })
+}
 </script>
 
 <template>
@@ -12,8 +27,9 @@ import Footer from '../components/Footer.vue'
   <Banner
     title="Transcript faster, better, cheaper"
     subtitle="More free time for you"
+    :login-with-google="loginWithGoogle"
   />
-  <Benefit />
+  <Benefit :login-with-google="loginWithGoogle" />
   <Pricing />
   <FAQ />
   <Footer />
